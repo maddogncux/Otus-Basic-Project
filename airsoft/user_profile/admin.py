56bf68666f72dec99be4f9a4cd1a4c2a27a4b5be
maddogncux <madddogncux@gmail.com>
2022-09-27 20:36:22 +0300

    1st commit add readme.md prepared django app done HomeWork7

---
+++ airsoft/user_profile/admin.py
@@ -0,0 +1,9 @@
+from typing import TYPE_CHECKING
+from django.contrib import admin
+from .models import Player
+if TYPE_CHECKING:
+    admin.site: admin.AdminSite
+
+# Register your models here.
+
+admin.site.register(Player)
---
+++ airsoft/user_profile/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Player(models.Model):
+    name = models.CharField(max_length=64)
+    camouflage = models.TextField(blank=True, null=True)
+
